47ea46de5fa75307be784f4b87739a9c5df6b31e
Kripa Jethwani <kripa@atliq.com>
2024-02-05 09:43:01 +0530

    flavour config changes

--- lib/config/flavour_config.dart
+++ lib/config/flavour_config.dart
@@ -6,4 +6,17 @@ class FlavorConfig {
       {required this.env, required this.appName, required this.apiBaseUrl});
 }
 
-enum Env { dev, prod, qa, staging }
+enum Env { dev, prod, staging }
+
+Map<Env, String>  apiBaseUrls= {
+  Env.dev: "https://dummyjson.com", //Replace with your Development API URL
+  Env.prod: "https://dummyjson.com", //Replace with your Production API URL
+  Env.staging: "https://dummyjson.com" //Replace with your Staging API URL
+};
+
+//Replace with your app names
+Map<Env, String>  appNames= {
+  Env.dev: "Development App",
+  Env.prod: "Production App",
+  Env.staging: "Staging App"
+};
